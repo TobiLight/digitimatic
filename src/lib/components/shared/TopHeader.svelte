f105990fee44c97c634d256aedc5db65329ddc31
TobiLight <jgetitdonefast@gmail.com>
2023-12-13 01:54:11 +0100

    digitmatic landing page

--- src/lib/components/shared/TopHeader.svelte
+++ src/lib/components/shared/TopHeader.svelte
@@ -4,16 +4,16 @@
 	import Youtube from '$lib/components/icons/Youtube.svelte';
 </script>
 
-<div class="top-header flex justify-between bg-white p-3 text-sm">
+<div class="top-header flex justify-between bg-gray-900 text-white p-3 text-sm">
 	<div class="mail">
-		<p class="text-gray-600">info@digitimatic.com</p>
+		<p class="text-gray-400">info@digitimatic.com</p>
 	</div>
 	<div class="social-links flex items-center gap-4">
-		<p class="text-gray-500">Follow us:</p>
+		<p class="text-gray-400">Follow us:</p>
 		<div class="flex gap-2 items-center">
 			<Facebook class="text-blue-600" />
 			<Youtube class="text-red-500" />
-			<Twitter class="text-black" />
+			<Twitter class="text-white" />
 		</div>
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 <header class="sticky top-0 z-[1]">
 	<TopHeader />
 	<nav
-		class="bg-white border-t px-3 py-5 flex items-center justify-between shadow-lg"
+		class="bg-black text-white border-t px-3 py-5 flex items-center justify-between shadow-lg"
 	>
 		<a on:click={() => isMenuOpen.set(false)} href="/" class="logo"
 			>DIGITIMATIC</a
@@ -41,7 +41,7 @@
 		id="mobileMenu"
 		class={`${
 			$isMenuOpen ? "left-0" : "left-[-100%]"
-		} bg-white fixed z-[2] border-t py-8 px-4 w-3/5 shadow-lg overflow-hidden h-full md:hidden transition-all duration-[.7s] ease-out`}
+		} bg-black text-white fixed z-[2] border-t py-8 px-4 w-3/5 shadow-lg overflow-hidden h-full md:hidden transition-all duration-[.7s] ease-out`}
 	>
 		<ul class="nav-links flex flex-col gap-14 h-full">
 			<li><a href="#who-we-are">About Us</a></li>
