00430d37b505a8dbe39cf4aa8f0e8e0ca2a8a086
TobiLight <jgetitdonefast@gmail.com>
2023-12-15 10:44:46 +0100

    ...

--- src/lib/components/Clients.svelte
+++ src/lib/components/Clients.svelte
@@ -12,152 +12,218 @@
 			provide our customers with an unwavering commitment to exceptional
 			service, ensuring an impressive 99.9% service reliability
 		</p>
-		<div class="grid grid-cols-3 gap-3 md:w-6/12 md:mx-auto items-center">
+		<div class="md:hdden grid gap-y-4">
 			<div
-				class="flex-none w-fit h-auto relative bg-gradient-to-r from-gray-200 to-gray-500 rounded-md"
+				class="flex justify-center gap-3 md:w-5/12 md:mx-auto items-center"
 			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/forbes.png"
-					alt=""
-					class="inset-0 w-32 h-[55px] mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
+				<figure
+					class="flex-none w-[120px] h-auto relative bg-gradient-to-r from-gray-200 to-gray-500 rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/forbes.png"
+						alt="Forbes"
+						class="inset-0 w-32 h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</figure>
+
+				<div
+					class="flex-none w-[120px] h-auto relative bg-white rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/dailymail.png"
+						alt="Daily Mail"
+						class="inset-0 w-32 h-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+
+				<div class="flex-none w-auto h-auto relative rounded-md">
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/yahoo.png"
+						alt="Yahoo"
+						class="inset-0 w-32 h-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
 			</div>
 
-			<div class="flex-none w-fit h-auto relative bg-white rounded-md">
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/dailynail.png"
-					alt=""
-					class="inset-0 w-32 h-[55px] object-cover rounded-lg"
-					loading="lazy"
-				/>
+			<div class="flex justify-center gap-3 md:w-5/12 md:mx-auto items-center">
+				<div
+					class="flex-none w-[120px] h-auto relative bg-gradient-to-r from-sky-500 to-indigo-500 rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/digital_journal.png"
+						alt="Digital Journal"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+
+				<div
+					class="flex-none w-[120px] h-auto relative bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/bloomberg.png"
+						alt="Bloomberg"
+						class="inset-0 px-2 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+
+				<div
+					class="flex-none w-[120px] h-auto relative bg-gradient-to-r from-gray-500 to-white rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/marketwatch.png"
+						alt=""
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
 			</div>
 
-			<div class="flex-none w-auto h-auto relative rounded-md">
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/yahoo.png"
-					alt=""
-					class="inset-0 w-32 h-[45px] sm:h-[55px] object-cover rounded-lg"
-					loading="lazy"
-				/>
+			<!-- <div
+				class="grid grid-cols-3 gap-3 md:w-5/12 md:mx-auto items-center"
+			>
+				<div class="flex-none w-auto h-auto relative rounded-md">
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/laweekly.jpg"
+						alt="LA Weekly"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+
+				<div
+					class="flex-none w-auto h-auto relative bg-gradient-to-r from-green-200 to-blue-300 rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/variety.png"
+						alt="Variety"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+
+				<div
+					class="flex-none w-auto h-auto relative bg-gradient-to-r from-gray-200 to-gray-500 rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/benzinga.png"
+						alt="Benzinga"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
 			</div>
+			<div
+				class="grid grid-cols-3 gap-3 md:w-5/12 md:mx-auto items-center"
+			>
+				<div
+					class="flex-none w-auto h-auto bg-gradient-to-r from-orange-200 to-red-300 relative rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/gq.png"
+						alt="GQ"
+						class="inset-0 w-[80px] h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+				<div class="flex-none w-auto h-auto relative rounded-md">
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/adweek.png"
+						alt="ADWEEK"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+				<div
+					class="flex-none w-auto h-auto relative bg-white rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/nasdaq.jpg"
+						alt="Nasdaq"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+			</div>
+
+			<div
+				class="grid grid-cols-2 gap-3 md:w-4/12 md:mx-auto items-center"
+			>
+				<div
+					class="flex-none w-auto h-auto px-3 relative bg-white rounded-md"
+				>
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/entrepreneur.png"
+						alt="Entrepreneur"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+				<div class="flex-none w-auto h-auto relative rounded-md">
+					<img
+						src="https://digitimatic.com/wp-content/uploads/2023/12/newyorkpost.png"
+						alt="New York Post"
+						class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
+						loading="lazy"
+					/>
+				</div>
+			</div> -->
 		</div>
 
-		<div class="grid grid-cols-3 gap-3 md:w-8/12 md:mx-auto items-center">
-			<div
-				class="flex-none w-auto h-auto relative bg-gradient-to-r from-sky-500 to-indigo-500 rounded-md"
-			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/digital_journal.png"
-					alt=""
-					class="inset-0 w-fit h-[40px] sm:h-[50px] mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-
-			<div
-				class="flex-none w-auto h-auto relative bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md"
-			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/bloomberg.png"
-					alt="Bloomberg"
-					class="inset-0 p-2 w-fit h-[40px] sm:h-[50px] mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-
-			<div
-				class="flex-none w-auto h-auto relative bg-gradient-to-r from-gray-500 to-white rounded-md"
-			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/marketwatch.png"
-					alt=""
-					class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-		</div>
-
-		<div class="grid grid-cols-3 gap-3 md:w-8/12 md:mx-auto items-center">
-			<div class="flex-none w-auto h-auto relative rounded-md">
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/laweekly.jpg"
-					alt=""
-					class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-
-			<div
-				class="flex-none w-auto h-auto relative bg-gradient-to-r from-green-200 to-blue-300 rounded-md"
-			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/variety.png"
-					alt=""
-					class="inset-0 w-fit h-auto lg:h-[75px] mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-
-			<div
-				class="flex-none w-auto h-auto relative bg-gradient-to-r from-gray-200 to-gray-500 rounded-md"
-			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/benzinga.png"
-					alt=""
-					class="inset-0 w-fit h-auto md:h-[75px] mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-		</div>
-		<div class="grid grid-cols-3 gap-3 md:w-10/12 md:mx-auto items-center">
-			<div
-				class="flex-none w-auto h-auto bg-gradient-to-r from-orange-200 to-red-300 relative rounded-md"
-			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/gq.png"
-					alt=""
-					class="inset-0 w-[80px] h-auto mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-			<div class="flex-none w-auto h-auto relative rounded-md">
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/adweek.png"
-					alt=""
-					class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-			<div class="flex-none w-auto h-auto relative bg-white rounded-md">
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/nasdaq.jpg"
-					alt=""
-					class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-		</div>
-
-		<div class="grid grid-cols-2 gap-3 md:w-10/12 md:mx-auto items-center">
-			<div
-				class="flex-none w-auto h-auto px-3 relative bg-white rounded-md"
-			>
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/entrepreneur.png"
-					alt=""
-					class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-			<div class="flex-none w-auto h-auto relative rounded-md">
-				<img
-					src="https://digitimatic.com/wp-content/uploads/2023/12/newyorkpost.png"
-					alt=""
-					class="inset-0 w-fit h-auto mx-auto object-cover rounded-lg"
-					loading="lazy"
-				/>
-			</div>
-		</div>
 	</div>
 </section>
+
+<style>
+	.gallery {
+		display: grid;
+		grid-template-columns: repeat(6, 1fr);
+		grid-template-rows: repeat(8, 5vw);
+		grid-gap: 15px;
+	}
+	.gallery__img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	.gallery__item--1 {
+		grid-column-start: 1;
+		grid-column-end: 1;
+		grid-row-start: 1;
+		grid-row-end: 3;
+	}
+
+	.gallery__item--2 {
+		grid-column-start: 2;
+		grid-column-end: 2;
+		grid-row-start: 1;
+		grid-row-end: 3;
+	}
+	.gallery__item--3 {
+		grid-column-start: 3;
+		grid-column-end: 3;
+		grid-row-start: 1;
+		grid-row-end: 3;
+	}
+	.gallery__item--4 {
+		grid-column-start: 4;
+		grid-column-end: 4;
+		grid-row-start: 1;
+		grid-row-end: 3;
+	}
+	.gallery__item--5 {
+		grid-column-start: 5;
+		grid-column-end: 5;
+		grid-row-start: 1;
+		grid-row-end: 3;
+	}
+	.gallery__item--6 {
+		grid-column-start: 6;
+		grid-column-end: 6;
+		grid-row-start: 1;
+		grid-row-end: 3;
+	}
+</style>
