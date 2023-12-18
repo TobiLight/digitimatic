1cf7390dc26f42fe0be1257373193b4b542e8ac6
TobiLight <jgetitdonefast@gmail.com>
2023-12-18 05:22:28 +0100

    ...

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -13,17 +13,10 @@ return {
 		client: {"start":"_app/immutable/entry/start.815f07d8.js","app":"_app/immutable/entry/app.79b365c8.js","imports":["_app/immutable/entry/start.815f07d8.js","_app/immutable/chunks/scheduler.8417489e.js","_app/immutable/chunks/singletons.765a1998.js","_app/immutable/chunks/index.b1b0aaf9.js","_app/immutable/entry/app.79b365c8.js","_app/immutable/chunks/scheduler.8417489e.js","_app/immutable/chunks/index.a5df3011.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
-			__memo(() => import('./nodes/1.js')),
-			__memo(() => import('./nodes/2.js'))
+			__memo(() => import('./nodes/1.js'))
 		],
 		routes: [
-			{
-				id: "/",
-				pattern: /^\/$/,
-				params: [],
-				page: { layouts: [0,], errors: [1,], leaf: 2 },
-				endpoint: null
-			}
+			
 		],
 		matchers: async () => {
 			
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.75e21ad3.js","_app/immutable/chunks/scheduler.8417489e.js","_app/immutable/chunks/index.a5df3011.js","_app/immutable/chunks/singletons.62dbc5a1.js","_app/immutable/chunks/index.b1b0aaf9.js"];
+export const imports = ["_app/immutable/nodes/1.1366a91d.js","_app/immutable/chunks/scheduler.8417489e.js","_app/immutable/chunks/index.a5df3011.js","_app/immutable/chunks/singletons.765a1998.js","_app/immutable/chunks/index.b1b0aaf9.js"];
 export const stylesheets = [];
 export const fonts = [];
