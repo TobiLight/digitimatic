a15aa7e87a751062d27d43a31d67c93c48d0f2fe
TobiLight <jgetitdonefast@gmail.com>
2023-12-18 04:39:09 +0100

    ...

--- src/lib/components/Slide.svelte
+++ src/lib/components/Slide.svelte
@@ -32,7 +32,7 @@
 	timer();
 </script>
 
-<div class={`${currentSlide === 1 ? 'h-[560px] mb-12' : 'h-[400px] mb-6'} slider relative md:h-fit`}>
+<div class={`${currentSlide === 1 ? 'h-[600px] mb-12' : 'h-[400px] mb-6'} slider relative md:h-fit`}>
 	<div class="slide">
 		{#each slides as slider, i}
 			{#if currentSlide === i}
