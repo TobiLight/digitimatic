807dafbd0a2713ad792b95785d410cd26d8cb937
TobiLight <jgetitdonefast@gmail.com>
2024-03-05 01:18:17 +0100

    ✔ chore: SEO keywords added to homepage

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import htmlMinifierAdaptor from "sveltekit-html-minifier";
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess({ postcss: true })],
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
